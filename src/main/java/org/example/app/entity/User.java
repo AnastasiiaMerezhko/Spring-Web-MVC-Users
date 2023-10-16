3e7b4d325fb31aa90e2a3d46748d972a2fea76af
Anastasiia <nastiamerezhko2003@gmail.com>
2023-10-16 21:44:27 +0200

    Add project

---
+++ src/main/java/org/example/app/entity/User.java
@@ -0,0 +1,25 @@
+package org.example.app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "lastName")
+    private String lastName;
+    @Column(name = "email")
+    private String email;
+}
